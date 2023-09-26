bb392ae4d3bcee07651e2f11d14f6820a4d5cbf1
abhishek-rabidas <avisec07@gmail.com>
2023-09-26 18:49:24 +0530

    ploygon draw for bounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {createRef, RefObject, useEffect, useState} from 'react';
 import './App.css';
-import { GoogleMap, useJsApiLoader} from "@react-google-maps/api";
+import {GoogleMap, Marker, Polygon, useJsApiLoader} from "@react-google-maps/api";
 import MapDrawingManager from "./MapDrawingManager";
 import MapRoute from "./MapRoute";
 import MapZoning from "./Zoning";
 
 function App() {
 
-    const { isLoaded } = useJsApiLoader({
+    const {isLoaded} = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
-        libraries:['drawing', 'places', 'routes']
+        libraries: ['drawing', 'places', 'routes']
     })
 
     const containerStyle = {
@@ -23,18 +23,64 @@ function App() {
         lng: 77.026344
     };
 
+    const inputRef:RefObject<HTMLInputElement> = createRef();
+
+    const [placeResponse, setPlaceResponse] = useState([])
+
+    const handleSubmit = () => {
+        const inputText = inputRef.current.value
+
+        const url = new URL('https://maps.googleapis.com/maps/api/geocode/json');
+
+        url.searchParams.append('address', inputText);
+        url.searchParams.append('key', process.env.REACT_APP_GOOGLE_MAP_API_KEY)
+
+        fetch(url).then(res => {
+            if (res.ok) {
+                return res.json()
+            }
+        }).then(data => {
+            console.log(data)
+            const northEast = data.results[0].geometry.viewport.northeast;
+            const southWest = data.results[0].geometry.viewport.southwest;
+
+            console.log(northEast)
+            console.log(southWest)
+
+            const northWest = {
+                lat: northEast.lat,
+                lng: southWest.lng
+            };
+
+            const southEast = {
+                lat: southWest.lat,
+                lng: northEast.lng
+            };
+
+            setPlaceResponse([northWest, northEast, southEast, southWest])
+        })
+
+    }
+
     return isLoaded ? (
         <>
-        <MapZoning />
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-        >
-            <MapRoute />
-        </GoogleMap>
+            <input type={'text'} placeholder={'Enter address'} ref={inputRef}/>
+            <button onClick={handleSubmit}>Submit</button>
+
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                <Marker label={"NW"} position={placeResponse.at(0)}></Marker>
+                <Marker label={"NE"} position={placeResponse.at(1)}></Marker>
+                <Marker label={"SE"} position={placeResponse.at(2)}></Marker>
+                <Marker label={"SW"} position={placeResponse.at(3)}></Marker>
+
+                <Polygon paths={placeResponse} options={{geodesic: false, fillColor: 'blue', fillOpacity: 0, strokeWeight: 0.3, strokeColor: 'blue'}}></Polygon>
+            </GoogleMap>
         </>
-    ): <></>
+    ) : <></>
 }
 
 export default App;
--- src/Zoning.tsx
+++ src/Zoning.tsx
@@ -1,31 +1,12 @@
-import {createRef, RefObject, useRef} from "react";
+import {createRef, RefObject, useRef, useState} from "react";
+import {OverlayView, Polygon} from "@react-google-maps/api";
 
 export default function MapZoning() {
 
-    const inputRef:RefObject<HTMLInputElement> = createRef();
 
-    const handleSubmit = () => {
-        const inputText = inputRef.current.value
-
-        const url = new URL('https://maps.googleapis.com/maps/api/geocode/json');
-
-       url.searchParams.append('address', inputText);
-       url.searchParams.append('key', process.env.REACT_APP_GOOGLE_MAP_API_KEY)
-
-        fetch(url).then(res => {
-            if (res.ok) {
-                return res.json()
-            }
-        }).then(data => {
-            console.log(data)
-        })
-
-    }
 
     return(<>
 
-        <input type={'text'} placeholder={'Enter address'} ref={inputRef}/>
-        <button onClick={handleSubmit}>Submit</button>
 
 
     </>)
